408ac9c0e8764b93f48b83c73f0bfc7de33679ac
Ekaterina <zvuchi89@mail.ru>
2022-09-14 17:42:44 +0300

    Задача23

---
+++ Задача23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+ Console.WriteLine(i  + "^3 = " + i * i * i );
